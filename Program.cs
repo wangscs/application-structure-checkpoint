1d1fe3d98e6a3b5a58a560ef23c281f2fca25c0e
Okinz <steven14812@gmail.com>
2021-05-10 14:40:12 -0400

    Built console application and added FluentAssertions Package

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace application_structure_checkpoint
+{
+  class Program
+  {
+    public void additionMethod()
+    {
+      var first = 3;
+      var second = 7;
+      var result = first + second;
+
+      result.Should().Be(10);
+    }
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+    }
+  }
+}
